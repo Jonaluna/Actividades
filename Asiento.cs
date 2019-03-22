154431ad6740cef43db112a5b13c5f21094e3440
Jonaluna <48217920+Jonaluna@users.noreply.github.com>
2019-03-22 13:03:51 -0600

    Add files via upload

---
+++ Asiento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace campionato.Modelo.Cinema
+{
+    class Asiento
+    {
+        private int _numero;
+        private bool _estaDisponible;
+
+
+        public int Numero { get; set; }
+        public bool EstaDisponible { get; set; }
+    }
+}
---
+++ Sala.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace campionato.Modelo.Cinema
+{
+    class Sala
+    {
+        private int _id;
+        private string _nombre;
+        private int _capacidad;
+        private bool _estaLLena;
+
+        private Asiento[] asiento = new Asiento[200];
+
+        public Sala()
+        {
+            _nombre = "A";
+
+            for (int i=0; i <=199; i++)
+            {
+                asiento[i] = new Asiento();
+                asiento[i].Numero = i + 1;
+            }
+
+            asiento[0] = new Asiento();
+            _capacidad = 200;
+            _estaLLena = false;
+        }
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Capacidad { get; set; }
+        public bool EstaLLena { get; set; }
+    }
+}
